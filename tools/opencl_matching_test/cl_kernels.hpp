9e9c06a0746f7e3241b1376c4dff042392625c23
Fabian Friederichs <fabian.friederichs@gmail.com>
2020-06-05 12:00:49 +0200

    CMake dependency management

---
+++ tools/opencl_matching_test/cl_kernels.hpp
@@ -0,0 +1,6 @@
+#ifndef _CL_KERNELS_H_
+#define _CL_KERNELS_H_
+
+
+
+#endif // !_CL_KERNELS_H_
--- tools/opencl_matching_test/opencl_matching_test.cpp
+++ tools/opencl_matching_test/opencl_matching_test.cpp
@@ -1,6 +1,21 @@
 #include <CL/cl2.hpp>
 
+#include <iostream>
+#include <vector>
+#include <cassert>
+
 int main()
 {
+	// test if opencl works
+	std::vector<cl::Platform> platforms;
+	cl::Platform::get(&platforms);
+	std::cout << "OpenCL Platforms found: " << platforms.size() << std::endl;
+	// print platform info
+	for(std::size_t p = 0; p < platforms.size(); ++p)
+	{
+		std::cout << "Platform " << p << ":" << std::endl;
+		std::cout << platforms[p].getInfo<CL_PLATFORM_VERSION>();
+	}
+
 	return 0;
 }
